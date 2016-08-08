7ca426be35eba8ba8ccc949df355b7a448a2df76
Emma <k54979802@gmail.com>
2016-08-08 07:41:29 +0000

    版面OK

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
-<?php
-header("content-type: text/html; charset=utf-8");
-?>
-<form method="POST">
+<meta charset="utf-8">
+<form method="POST" action="detail.php">
     <label for="account">帳戶名稱：</label>
     <input type="text" id="account" name="account"/>
     <input type="submit" name="submit"/>

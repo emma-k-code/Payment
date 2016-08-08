f16fa8a6d67c2337e615864562d486d975aa1a83
Emma <k54979802@gmail.com>
2016-08-08 08:19:42 +0000

    可轉出與轉入帳戶金額

--- GetAccount.php
+++ GetAccount.php
@@ -17,6 +17,27 @@ class GetAccount extends Database
         
         return $result->fetch();
     }
+    
+    public function insert($io, $account, $money, $now)
+    {
+        if ($io == "out") {
+            $money = -$money;
+        }
+        
+        $sql = "INSERT INTO `details`(`account`, `datetime`, `transaction`) VALUES (:account, :now, :money)";
+        $sth = $this->prepare($sql);
+        $sth->bindParam("account",$account);
+        $sth->bindParam("now",$now);
+        $sth->bindParam("money",$money);
+        $sth->execute();
+        
+        $sql = "UPDATE `account` SET `balance` = `balance` + (:money) WHERE `account` = :account";
+        $sth = $this->prepare($sql);
+        $sth->bindParam("account",$account);
+        $sth->bindParam("money",$money);
+        $sth->execute();
+        
+    }
 }
 
 ?>
--- detail.php
+++ detail.php
@@ -1,11 +1,25 @@
 <?php
 
-if (isset($_POST['submit'])) {
-    $account = addslashes($_POST['account']);
-    require_once "GetAccount.php";
-    $getData = new GetAccount;
-    $accountData = $getData->search($account);
+require_once "GetAccount.php";
+
+date_default_timezone_set("Asia/Taipei");
+$now = date('Y-m-d H:i:s');
+$account = addslashes($_POST['account']);
+
+$getData = new GetAccount;
+
+if (isset($_POST['enterSubmit'])) {
+    $money = addslashes($_POST['enter']);
+    $getData->insert("enter", $account, $money, $now);
 }
+
+if (isset($_POST['outSubmit'])) {
+    $money = addslashes($_POST['out']);
+    $getData->insert("out", $account, $money, $now);
+}
+
+$accountData = $getData->search($account);
+
 ?>
 <meta charset="utf-8">
 <h2><?php echo $accountData[0];?></h2>
@@ -19,6 +33,8 @@ if (isset($_POST['submit'])) {
     <label for="out">輸入金額：</label>
     <input type="number" id="out" name="out"/>
     <input type="submit" min="0" name="outSubmit" value="轉出"/>
+    
+    <input type="hidden" name="account" value="<?php echo $accountData[0];?>"/>
 </form>
 
 <div>

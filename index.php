b1c2126f42ffc82ac2280510126f7d9aa2cf0d56
Emma <k54979802@gmail.com>
2016-08-08 06:52:37 +0000

    完成帳戶輸入頁面

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+header("content-type: text/html; charset=utf-8");
+?>
+
+<form>
+    <label for="account">帳戶名稱：</label>
+    <input type="text" id="account" name="account"/>
+    <input type="submit" name="submit"/>
+</form>

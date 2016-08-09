506f61268020d0391b4d727748c3da097b55882e
Emma <k54979802@gmail.com>
2016-08-09 06:51:30 +0000

    刪除多餘空白

--- Account.php
+++ Account.php
@@ -8,7 +8,7 @@ require_once "Database.php";
 
 class Account extends Database
 {
-    
+
     public function search($account)
     {
         $sql = "SELECT * FROM `account` WHERE `account` = :account";
@@ -18,7 +18,7 @@ class Account extends Database
         
         return $result->fetch();
     }
-    
+
     public function searchDetail($account)
     {
         $sql = "SELECT * FROM `details` WHERE `account` = :account ORDER BY `datetime` DESC";
@@ -28,23 +28,23 @@ class Account extends Database
         
         return $result->fetchAll();
     }
-    
+
     public function insert($io, $account, $money, $now)
     {
         if ($io == "out") {
             $money = -$money;
         }
-        
+
         try {
             $this->transaction();
-            
+
             $sql = "SELECT * FROM `account` WHERE `account` = :account";
             $result = $this->prepare($sql);
             $result->bindParam("account", $account);
             $result->execute();
-            
+
             $accountData = $result->fetch();
-            
+
             $balance = $accountData[1] + $money;
             $version = $accountData[2];
             if ($balance < 0) {
@@ -62,7 +62,7 @@ class Account extends Database
             if ($sth->rowCount() == 0) {
                 throw new Exception("交易失敗");
             }
-            
+
             $sql = "INSERT INTO `details`(`account`, `datetime`, `transaction`) VALUES (:account, :now, :money)";
             $sth = $this->prepare($sql);
             $sth->bindParam("account", $account);
@@ -71,15 +71,15 @@ class Account extends Database
             if (!$sth->execute()) {
                 throw new Exception("交易失敗");
             }
-            
+
             $this->commit();
-            
+
         } catch(Exception $e) {
             $this->rollBack();
             $error = $e->getMessage();
         }
-        
+
         return $error;
     }
-    
+
 }
--- Database.php
+++ Database.php
@@ -6,9 +6,9 @@ class Database
     const DATABASE_NAME = 'payment';
     const DATABASE_USERNAME = 'root';
     const DATABASE_PASSWORD = '';
-    
+
     private $connection = null;
-    
+
     public function __construct()
     {
         $dsn = sprintf('mysql:dbname=%s;host=%s', static::DATABASE_NAME, static::DATABASE_HOST);
@@ -19,7 +19,7 @@ class Database
             echo 'Connection failed: '.$e->getMessage();
         }
     }
-    
+
     /**
      * @param   string  SQL query
      * @return  object  
@@ -28,17 +28,17 @@ class Database
     {
         return $this->connection->prepare($sql);
     }
-    
+
     public function transaction()
     {
         $this->connection->beginTransaction();
     }
-    
+
     public function commit()
     {
         $this->connection->commit();
     }
-    
+
     public function rollBack()
     {
         $this->connection->rollBack();

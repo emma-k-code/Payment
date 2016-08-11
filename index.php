54eb9e0d58c825c5a96e4e2bd95670817520d28d
Emma <k54979802@gmail.com>
2016-08-11 02:06:16 +0000

    更改檔名

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8">
-<form method="POST" action="detail.php">
+<form method="POST" action="Detail.php">
     <label for="account">帳戶名稱：</label>
     <input type="text" id="account" name="account"/>
     <input type="submit" name="submit"/>
